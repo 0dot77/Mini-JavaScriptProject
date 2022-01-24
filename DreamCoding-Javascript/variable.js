7a0f178eda745d4ea0d19b169187329c4d290373
TAEYANG YOO <yty0706@gmail.com>
2022-01-24 22:32:36 +0900

    0124

---
+++ DreamCoding-Javascript/variable.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+// 4. Variable Types
+// Primitive, single item : number, string, boolean, null, undefine, symbol
+// object, box container
+// function, first-class function
+
+const count = 17; // integer
+const size = 17.1; // decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// number - special numeric values : infinity, -infinity, NaN
+
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt (fairly new, don't use it yet.)
+// Chrome and Firefox에만 추가 되어있다.
+const bigInt = 129349248190248912489259210491;
+console.log(`value: ${bigInt}, tpye: ${typeof bigInt}`);
+Number.MAX_SAFE_INTEGER;
+
+// string
+const char ='c';
+const brendan = 'brendan';
+const greeting = 'hello ' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`; // template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true : any other value
+
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
