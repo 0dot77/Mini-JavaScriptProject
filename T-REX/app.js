db777917eac1db075279acb4a966365fa36852b9
Soleil- <yty0706@gmail.com>
2022-01-28 18:05:11 +0900

    fix

--- T-REX/app.js
+++ T-REX/app.js
@@ -13,11 +13,14 @@ const dino = document.querySelector('.dino');
 const grid = document.querySelector('.grid');
 const alert = document.getElementById('alert');
 
+// 점프를 하는지 안하는지 체크
 let isJumping = false;
 
 // 점프해서 내려가는 것을 조금 더 자연스럽게 연출하기 위한 약간의 중력 적용
 let gravity = 0.9;
+
 let isGameOver = false;
+let score;
 
 function control(e) {
     //keyCode 를 32로 설정해서, space가 눌리면 점프 하도록 함
@@ -33,13 +36,18 @@ function control(e) {
 document.addEventListener('keyup', control);
 
 let position = 0;
+let lastPosition = 0;
 
+// 점프를 구현하는 함수
 function jump() {
     let count = 0;
     let timerId = setInterval(function () {
 
-        //move down
-        if (count  === 15) {
+        // 점프 후 낙하
+        if (count === 15) {
+            // timerId를 선언해서 setInterval() 함수를 할당한 이유는 clearInterval()을 활용해서
+            // 초기화시켜주기 위함이다. count가 15가 되면 timerId의 함수 호출을 멈추고
+            // downTimerId의 함수 호출을 지속한다.
             clearInterval(timerId);
             let downTimerId = setInterval(function () {
                 if(count === 0) {
@@ -53,11 +61,12 @@ function jump() {
             },20);
         }
 
-        //move up
+        //점프
         position += 30;
         count++;
         position *= gravity;
         dino.style.bottom = position + 'px';
+        console.log(count);
     },20);
 }
 
@@ -66,6 +75,8 @@ function generateObstacle() {
     let randomTime = Math.random() * 4000;
     let obstaclePosition = 1000;
     const obstacle = document.createElement('div');
+
+    //게임이 끝나지 않았다면,
     if(!isGameOver) obstacle.classList.add('obstacle');
     grid.appendChild(obstacle);
 
@@ -73,27 +84,27 @@ function generateObstacle() {
     obstacle.style.left = obstaclePosition + 'px';
 
     let timerId = setInterval(function() {
+
         if(obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
             // alert가 울리는 포지션을, 장애물과 부딪혔을 때의 위치로 두고 싶다.
             // 문제 - 일정 시간이 지나면 포지션 위치가 초기화 된다.
             let alertPosition = position;
-            clearInterval(timerId);
             alert.innerText = 'GAME OVER';
             alert.style.bottom = alertPosition + 'px';
             isGameOver = true;
             console.log(alertPosition);
+            clearInterval(timerId);
 
             //오브젝트가 장애물에 부딪히면 모든 div를 없애준다.
             while(grid.firstChild) {
                 grid.removeChild(grid.lastChild);
             }
         }
-
-
         obstaclePosition -= 10;
         obstacle.style.left = obstaclePosition + 'px';
     },20);
+
+    //setTimeout(), randomTime 이 흐르면 장애물을 하나씩 만든다.
     if(!isGameOver) setTimeout(generateObstacle, randomTime);
 }
-
 generateObstacle();
