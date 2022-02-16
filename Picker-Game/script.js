66d73023292894b46e95f86ad6b0f5c821d5c5aa
TAEYANG YOO <yty0706@gmail.com>
2022-02-16 11:09:47 +0900

    picker game complete

--- Picker-Game/script.js
+++ Picker-Game/script.js
@@ -1,6 +1,42 @@
 "use strict";
 
 // 기능
-// - 각 picker를 누르면 팝업창이 뜬다. 팝업창에는, 내가 픽한 수 그리고 픽해야 하는 수를 대조해서 보여준다. 일치한다면 cong, 일치하지 않으면 cheer up
-// - 카드를 까는 것처럼 누른 것들은 계속 누른 상태를 유지하며, 선택된 수를 보여준다.
-// - score는 몇번 만에 맞춰서 성공했는지를 기억하기
+// - 각 Picker를 눌렀을 때 Pick -> 랜덤한 숫자로 바뀌기 (1-9)
+
+// goal에 1-9까지의 목표 값 할당하기
+const goalNumber = document.querySelector(".goal");
+const picker = document.querySelectorAll(".picker");
+const againBtn = document.querySelector(".again");
+const titleBlur = document.querySelector(".blur");
+
+const goalRandomNumber = function () {
+  goalNumber.textContent = Math.trunc(Math.random() * 9 + 1);
+};
+
+goalRandomNumber();
+
+for (let i = 0; i < picker.length; i++) {
+  const generateRandomNumber = Math.trunc(Math.random() * 9 + 1);
+  picker[i].classList.remove("picked");
+  picker[i].addEventListener("click", function () {
+    titleBlur.classList.remove("blur");
+    picker[i].textContent = generateRandomNumber;
+    picker[i].classList.add("picked");
+    if (generateRandomNumber === Number(goalNumber.textContent)) {
+      alert("You Win!");
+      location.reload();
+    }
+  });
+}
+
+// 버튼을 눌렀을 때, pick이 아닌 상태에 있는 것들을 다시 pick으로 되돌린다.
+againBtn.addEventListener("click", function () {
+  goalRandomNumber();
+  titleBlur.classList.add("blur");
+  for (let i = 0; i < picker.length; i++) {
+    if (picker[i].classList.contains("picked")) {
+      picker[i].classList.remove("picked");
+      picker[i].textContent = "pick";
+    }
+  }
+});
